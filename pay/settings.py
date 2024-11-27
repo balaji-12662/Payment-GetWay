9c1efbc17e85d18054e9af2dbdf9e7dcc675df76
balaji-12662 <balaji@hola9.com>
2024-11-27 10:44:54 +0530

    deploy this project

--- pay/settings.py
+++ pay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m5f%@l%!uu=3dh4b)@g+r27dvuj%sim5uxn2*m#npv_=8s^%73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
